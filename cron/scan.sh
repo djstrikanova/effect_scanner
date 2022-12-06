c28333e7883e9956e6ede407b084e69f33a3b090
DJSTRIKANOVA <efx_builder@protonmail.com>
2022-12-05 22:36:35 -0600

    cron jobs for repeated checks

---
+++ cron/scan.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# Load nvm
+export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+nvm use 19.2.0
+node ~/effect_scanner/scan_simple.js
